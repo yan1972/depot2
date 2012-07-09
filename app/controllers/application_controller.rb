b7fb04be73c26bd94dfa47bb29b2ccd2a23d7b23
chris yan <yan972@yahoo.com>
2012-07-09 15:09:36 +0800

    loging in ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,13 +10,12 @@ class ApplicationController < ActionController::Base
   end
   
   private
-
-    def current_cart 
-      Cart.find(session[:cart_id])
-    rescue ActiveRecord::RecordNotFound
-      cart = Cart.create
-      session[:cart_id] = cart.id
-      cart
-    end
+  def current_cart 
+    Cart.find(session[:cart_id])
+  rescue ActiveRecord::RecordNotFound
+    cart = Cart.create
+    session[:cart_id] = cart.id
+    cart
+  end
 end
 
