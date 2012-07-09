1763de86cbf40e76e5b965769b2fdb7af3e15797
chris yan <yan1972@gmail.com>
2012-07-10 00:09:20 +0800

    15.3 Iteration J3: Translating Checkout

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -61,7 +61,7 @@ class OrdersController < ApplicationController
         
         OrderNotifier.received(@order).deliver
         
-        format.html { redirect_to store_url, notice: 'Thank you for your order.' }
+        format.html { redirect_to store_url, notice: I18n.t('.thanks') }
         format.json { render json: @order, status: :created, location: @order }
       else
         @cart = current_cart
